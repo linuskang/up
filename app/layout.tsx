a1ec08f3e2da0bd75f432cdbfa9d271d49e95078
linuskang <linus@kang.id.au>
2026-06-12 22:05:14 +1000

    Add small footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,27 +4,30 @@ import { GeistSans } from "geist/font/sans"
 import "./globals.css"
 import { Toaster } from "@/components/ui/sonner"
 export const metadata = {
-  title: "Upstream",
-  description: "A simple logging platform built for developers.",
+    title: "Upstream",
+    description: "A simple logging platform built for developers.",
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`flex min-h-screen flex-col bg-background ${GeistSans.className}`}>
-        <main>
-          <ThemeProvider attribute="class" defaultTheme="dark">
-            <TooltipProvider>
-              {children}
-              <Toaster richColors position="top-center" />
-            </TooltipProvider>
-          </ThemeProvider>
-        </main>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={`flex min-h-screen flex-col bg-background ${GeistSans.className}`}>
+                <main className="flex-1">
+                    <ThemeProvider attribute="class" defaultTheme="dark">
+                        <TooltipProvider>
+                            {children}
+                            <Toaster richColors position="top-center" />
+                        </TooltipProvider>
+                    </ThemeProvider>
+                </main>
+                <footer className="w-full py-4 text-center text-xs text-muted-foreground">
+                    Upstream is v0.2.0. Project by Linus Kang.
+                </footer>
+            </body>
+        </html>
+    )
 }
