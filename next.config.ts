fedd4c7865ad6ca54dfc69eee4a6a45166c94725
linuskang <linus@kang.id.au>
2026-06-18 17:31:56 +1000

    Update devserver stuff to localhost.linus.my

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
     output: "standalone",
     allowedDevOrigins: [
-        "10.0.0.194",
+        "localhost.linus.my",
     ],
 }
 
