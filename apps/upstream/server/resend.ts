4aeae3e7ee4357d74fec4e7b4a1659456de0259c
linuskang <linus@kang.id.au>
2026-07-06 11:15:10 +1000

    fix resend

--- apps/upstream/server/resend.ts
+++ apps/upstream/server/resend.ts
@@ -1,7 +1,9 @@
 import { Resend } from "resend"
 import { env } from "@/env"
 
-const resend = new Resend(env.RESEND_API_KEY)
+function ResendClient() {
+    return new Resend(env.RESEND_API_KEY)
+}
 
 export class Email {
     static async send(
@@ -10,7 +12,7 @@ export class Email {
         text: string,
         html?: string
     ) {
-        await resend.emails.send({
+        await ResendClient().emails.send({
             from: env.RESEND_EMAIL_FROM,
             to,
             subject,
